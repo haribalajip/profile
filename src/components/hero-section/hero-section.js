3c3ef790495f2ed47f87472c3440ce7d48d2206d
hari balaji <hari111.balaji@gmail.com>
2021-04-01 23:08:42 +0530

    chore: tab title change

--- src/components/hero-section/hero-section.js
+++ src/components/hero-section/hero-section.js
@@ -43,7 +43,9 @@ const HeroSection = (props) => {
                 <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt=""></img>
               </div>
               <div className="-mr-2">
-                <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <button
+                  type="button"
+                  className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className="sr-only">Close main menu</span>
                   <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -87,7 +89,7 @@ const HeroSection = (props) => {
       </div>
     </div>
     <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-      <img className="w-2/3 h-2/3 mx-auto mt-20 md:w-1/2 md:w-1/2" src="hero.svg" alt=""/>
+      <img className="w-2/3 h-2/3 mx-auto mt-20 md:w-1/2 md:w-1/2" src="hero.svg" alt="" title="Psst! My desk will have a lot of toys unlike this one :P"/>
     </div>
   </div>)
 }
