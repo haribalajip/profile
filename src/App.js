91e6e43bd99a21a9d209bffba7e6bdbb458a0d4f
hari balaji <hari111.balaji@gmail.com>
2021-04-03 09:10:13 +0530

    feat: added my routine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css'; 
 import HeroSection from './components/hero-section/hero-section'
+import FeatureSection from './components/feature-section/feature-section'
 import { useState } from 'react';
 import Constants from './constants/constants'
 const App = () => {
   const [ heroSectionState ]  = useState({
     heroSectionConfig: Constants.heroSectionConfig
-  })
+  });
+  const [ featureSectionState ]  = useState({
+    featureSectionConfig: Constants.featureSectionConfig
+  });
   return (
-  <HeroSection config={heroSectionState.heroSectionConfig}></HeroSection>
+  <div>
+    <HeroSection config={heroSectionState.heroSectionConfig}></HeroSection>
+    <FeatureSection config={featureSectionState.featureSectionConfig}></FeatureSection>
+  </div>
   );
 }
 
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,8 +8,34 @@ const constants = {
     ],
     greet: "I'm, ",
     name: "Hari Balaji",
-    subtitle: "A frontend developer and a JS enthusiast building awesome features for Freshworks.",
+    subtitle: "A frontend developer and a JS enthusiast building awesome features for Freshworks CRM.",
     linkedInUrl: "https://www.linkedin.com/in/haribalajip/"
+  },
+  featureSectionConfig: {
+    titleSmall: "MY LIFE",
+    title: "My daily routine",
+    list: [
+      {
+        svgName: 'CodeIcon',
+        title: "Build Email features",
+        description: "Email is a crucial area for a CRM. And I take care of Frontend engineering for email and email related features."
+      },
+      {
+        svgName: 'AnnotationIcon',
+        title: "Do code reviews for peers",
+        description: "I ensure that the code shipped from our squad is of world class quality. I also make sure that the code conforms to our product's coding standards."
+      },
+      {
+        svgName: 'UserGroupIcon',
+        title: "Collaborate",
+        description: "My favorite part. I engage with my team in conversations where my advice makes value. Often, I see myself mentoring a few new joiners."
+      },
+      {
+        svgName: 'HeartIcon',
+        title: 'Run a mile',
+        description: "I love monrings. And I love running. What more else one would would ask when the weather is great for a morning run! I make sure I do one mile run atleast 4 days a week."
+      }
+    ]
   }
 };
 export default constants;
