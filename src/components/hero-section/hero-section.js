c7adf39e68190d61ea16abb2ff57695408cd04b5
Hariharasuthan B <hari111.balaji@gmail.com>
2022-08-28 19:50:53 +0530

    feat: open freshchat

--- src/components/hero-section/hero-section.js
+++ src/components/hero-section/hero-section.js
@@ -9,6 +9,11 @@ const HeroSection = (props) => {
   const handleCloseBtn = () => {
     setVisibilityState({ showMobileNav: !visibilityState.showMobileNav });
   }
+
+  const dropMessage = () => {
+    window.fcWidget ? window.fcWidget.open() : setTimeout(() => window.fcWidget.open(), 1000);
+  }
+
   return (<div className="relative bg-white overflow-hidden">
     <div className="max-w-7xl mx-auto">
       <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
@@ -82,9 +87,9 @@ const HeroSection = (props) => {
             </p>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="rounded-md shadow">
-                <a href={props.config.linkedInUrl} className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                  Get connected
-                </a>
+                <div onClick={dropMessage} className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                  Drop a message
+                </div>
               </div>
               <div className="mt-3 sm:mt-0 sm:ml-3">
                 {/* <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
