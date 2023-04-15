06bd6f8c667e7c0ae2919789b2f19bef65b34f52
Hariharasuthan B <hari111.balaji@gmail.com>
2023-04-15 09:40:00 +0530

    chore: hero-section migrated to ts

---
+++ src/components/hero-section/hero-section.tsx
@@ -0,0 +1,177 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import LazyImage from "../lazy-image/component";
+import { useState } from "react";
+import constants from "../../constants/constants";
+
+const HeroSection: React.FC<{ config: typeof constants.heroSectionConfig }> = (
+  props
+) => {
+  const [visibilityState, setVisibilityState] = useState({
+    showMobileNav: false,
+  });
+  const handleCloseBtn = () => {
+    setVisibilityState({ showMobileNav: !visibilityState.showMobileNav });
+  };
+
+  const dropMessage = () => {
+    // @ts-expect-error
+    window.fcWidget // @ts-expect-error
+      ? window.fcWidget.open() // @ts-expect-error
+      : setTimeout(() => window.fcWidget.open(), 1000);
+  };
+
+  return (
+    <div className="relative bg-white overflow-hidden">
+      <div className="max-w-7xl mx-auto">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+            <nav
+              className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+              aria-label="Global"
+            >
+              <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                <div className="flex items-center justify-between w-full md:w-auto">
+                  <a href="#">
+                    <span className="sr-only">Workflow</span>
+                    <img
+                      className="h-8 w-auto sm:h-10 lg:h-14"
+                      src="haribalaji.svg"
+                    ></img>
+                  </a>
+                  <div className="-mr-2 flex items-center md:hidden">
+                    <button
+                      onClick={handleCloseBtn}
+                      type="button"
+                      className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                      aria-expanded="false"
+                    >
+                      <span className="sr-only">Open main menu</span>
+                      <svg
+                        className="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                {props.config.links.map((item) => {
+                  return (
+                    <a
+                      href={item.url}
+                      key={item.url}
+                      className="font-medium text-gray-500 hover:text-gray-900"
+                    >
+                      {item.label}
+                    </a>
+                  );
+                })}
+              </div>
+            </nav>
+          </div>
+
+          <div
+            className={
+              visibilityState.showMobileNav
+                ? "absolute top-0 inset-x-0 p-2 transition transform origin-top-right"
+                : "hidden"
+            }
+          >
+            <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+              <div className="px-5 pt-4 flex items-center justify-between">
+                <div>
+                  <img className="h-8 w-auto" src="haribalaji.svg" alt=""></img>
+                </div>
+                <div className="-mr-2">
+                  <button
+                    onClick={handleCloseBtn}
+                    type="button"
+                    className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                  >
+                    <span className="sr-only">Close main menu</span>
+                    <svg
+                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <div className="px-2 pt-2 pb-3 space-y-1">
+                {props.config.links.map((item) => {
+                  return (
+                    <a
+                      href={item.url}
+                      key={item.url}
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.label}
+                    </a>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-20 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-5xl">
+                <span className="block xl:inline">{props.config.greet}</span>
+                <span className="block text-indigo-600 xl:inline">
+                  {props.config.name}
+                </span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                {props.config.subtitle}
+              </p>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                <div className="rounded-md shadow">
+                  <div
+                    onClick={dropMessage}
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                  >
+                    Drop a message
+                  </div>
+                </div>
+                <div className="mt-3 sm:mt-0 sm:ml-3">
+                  {/* <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                  Live demo
+               </a> */}
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <LazyImage
+          className="w-1/3 h-2/3 mx-auto md:mt-20 md:w-1/2 md:w-1/2"
+          src="hero.svg"
+        />
+      </div>
+    </div>
+  );
+};
+export default HeroSection;
